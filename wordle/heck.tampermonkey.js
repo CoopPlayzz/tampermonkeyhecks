be7b151cd61dcaeccd76fafcbbc3d36b58ee5863
CoopPlayzz <cooperschwarting@icloud.com>
2022-04-18 10:44:04 -0500

    update

---
+++ wordle/heck.tampermonkey.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name Wordle Heck
+// @version 1.0
+// @description A  Wordle Hack that enters the solution for u
+// @author CoopPlayzz
+// @homepage https://github.com/CoopPlayzz/tampermonkeyhecks
+// @updateURL 
+// @downloadURL 
+// @include https://www.nytimes.com/games/wordle/*
+// ==/UserScript==
