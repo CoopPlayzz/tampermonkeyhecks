9a62d4fab0c25a53dff07fa7b2aba2dee06661db
CoopPlayzz <cooperschwarting@icloud.com>
2022-04-18 10:52:59 -0500

    update 1 for tampermonkey wordle

--- wordle/heck.tampermonkey.js
+++ wordle/heck.tampermonkey.js
@@ -4,7 +4,7 @@
 // @description A  Wordle Hack that enters the solution for u
 // @author CoopPlayzz
 // @homepage https://github.com/CoopPlayzz/tampermonkeyhecks
-// @updateURL 
-// @downloadURL 
+// @updateURL https://raw.githubusercontent.com/CoopPlayzz/tampermonkeyhecks/main/wordle/heck.tampermonkey.js
+// @downloadURL https://raw.githubusercontent.com/CoopPlayzz/tampermonkeyhecks/main/wordle/heck.tampermonkey.js
 // @include https://www.nytimes.com/games/wordle/*
 // ==/UserScript==
